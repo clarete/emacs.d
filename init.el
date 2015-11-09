add9b9880ad050cde9e8bccaa23496e63e3d5c31
Lincoln de Sousa <lincoln@comum.org>
2015-11-08 20:45:49 -0500

    web-mode: set markup,css,code indent offset to 2 & add jsx to auto-mode-alist

--- init.el
+++ init.el
@@ -184,9 +184,9 @@
 (require 'web-mode)
 (defun web-mode-hook ()
   "Hooks for Web mode."
-  (setq web-mode-markup-indent-offset 4)
-  (setq web-mode-css-indent-offset 4)
-  (setq web-mode-code-indent-offset 4)
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
   (setq web-mode-enable-current-element-highlight t)
   (setq web-mode-enable-current-column-highlight t)
   (set-face-attribute 'web-mode-doctype-face nil :foreground
@@ -196,6 +196,7 @@
   (set-face-attribute 'web-mode-html-attr-value-face nil :foreground
                       (face-foreground font-lock-type-face)))
 (add-hook 'web-mode-hook  'web-mode-hook)
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
