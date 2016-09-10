0e1dbf119657593888e2678cf708d4deedba6e80
Lincoln de Sousa <lincoln@comum.org>
2016-09-10 01:19:01 -0400

    Better experience with Asynchronous Shell Commands
     * Don't ask if a new frame should be created if the default name is
       taken, just use a new name.
     * Don't display the output buffer for asynchronous shell * commands;

--- lisp/custom-exwm-config.el
+++ lisp/custom-exwm-config.el
@@ -9,27 +9,42 @@
 
 (defun custom-exwm-config ()
   "Custom configuration of EXWM."
+
+  ;; Don't ask if a new buffer should be created. Just do it!
+  (setq async-shell-command-buffer 'new-buffer)
+
+  ;; Don't show buffer with output for asynchronous commands
+  (add-to-list
+   'display-buffer-alist
+   '(".*Async Shell Command.*" (display-buffer-no-window)))
+
   ;; Set the initial workspace number.
   (setq exwm-workspace-number 4)
+
   ;; Make class name the buffer name
   (add-hook 'exwm-update-class-hook
             (lambda ()
               (exwm-workspace-rename-buffer exwm-class-name)))
+
   ;; 's-r': Reset
   (exwm-input-set-key (kbd "s-r") #'exwm-reset)
+
   ;; 's-w': Switch workspace
   (exwm-input-set-key (kbd "s-w") #'exwm-workspace-switch)
+
   ;; 's-N': Switch to certain workspace
   (dotimes (i 10)
     (exwm-input-set-key (kbd (format "s-%d" i))
                         `(lambda ()
                            (interactive)
                            (exwm-workspace-switch-create ,i))))
+
   ;; 's-&': Launch application
   (exwm-input-set-key (kbd "s-&")
                       (lambda (command)
                         (interactive (list (read-shell-command "$ ")))
                         (start-process-shell-command command nil command)))
+
   ;; Line-editing shortcuts
   (exwm-input-set-simulation-keys
    '(([?\C-b] . left)
