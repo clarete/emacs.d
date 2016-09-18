1eaab1199cb129d8d11f5f4609a522b44f4576f3
Lincoln de Sousa <lincoln@comum.org>
2016-09-18 08:56:12 -0400

    exwm: Split configuration into smaller functions

--- lisp/custom-exwm-config.el
+++ lisp/custom-exwm-config.el
@@ -7,24 +7,9 @@
 
 (require 'exwm)
 
-(defun custom-exwm-config ()
-  "Custom configuration of EXWM."
 
-  ;; Don't ask if a new buffer should be created. Just do it!
-  (setq async-shell-command-buffer 'new-buffer)
-
-  ;; Don't show buffer with output for asynchronous commands
-  (add-to-list
-   'display-buffer-alist
-   '(".*Async Shell Command.*" (display-buffer-no-window)))
-
-  ;; Set the initial workspace number.
-  (setq exwm-workspace-number 4)
-
-  ;; Make class name the buffer name
-  (add-hook 'exwm-update-class-hook
-            (lambda ()
-              (exwm-workspace-rename-buffer exwm-class-name)))
+(defun custom-exwm-config-global-keys ()
+  "Configure Keybindings for exwm."
 
   ;; Bind window cycling to Meta-Tab & Meta-Shift-Tab
   (exwm-input-set-key (kbd "<M-tab>") #'other-window)
@@ -32,15 +17,15 @@
    (kbd "<M-iso-lefttab>")
    #'(lambda () (interactive) (other-window -1)))
 
+  ;; Unbind <M-tab> when in magit
+  (with-eval-after-load 'magit-mode
+    (define-key magit-mode-map (kbd "<M-tab>") nil))
+
   ;; Bind keys for resizing windows
   (exwm-input-set-key (kbd "C-{") #'shrink-window-horizontally)
   (exwm-input-set-key (kbd "C-}") #'enlarge-window-horizontally)
   (exwm-input-set-key (kbd "C-M-}") 'shrink-window)
   (exwm-input-set-key (kbd "C-M-{") 'enlarge-window)
-
-  ;; Unbind <M-tab> when in magit
-  (with-eval-after-load 'magit-mode
-    (define-key magit-mode-map (kbd "<M-tab>") nil))
 
   ;; 's-r': Reset
   (exwm-input-set-key (kbd "s-r") #'exwm-reset)
@@ -49,7 +34,7 @@
   (exwm-input-set-key (kbd "s-w") #'exwm-workspace-switch)
 
   ;; 's-N': Switch to certain workspace
-  (dotimes (i 4)
+  (dotimes (i exwm-workspace-number)
     (exwm-input-set-key (kbd (format "s-%d" (1+ i)))
                         `(lambda ()
                            (interactive)
@@ -59,7 +44,11 @@
   (exwm-input-set-key (kbd "s-&")
                       (lambda (command)
                         (interactive (list (read-shell-command "$ ")))
-                        (start-process-shell-command command nil command)))
+                        (start-process-shell-command command nil command))))
+
+
+(defun custom-exwm-config-editing-keys ()
+  "Configure editing keys for apps under exwm."
 
   ;; Line-editing shortcuts
   (exwm-input-set-simulation-keys
@@ -72,10 +61,40 @@
      ([?\M-v] . prior)
      ([?\C-v] . next)
      ([?\C-d] . delete)
-     ([?\C-k] . (S-end delete))))
+     ([?\C-k] . (S-end delete)))))
 
-  ;; Enable EXWM
+
+(defun custom-exwm-config-shell-command ()
+  "Configure Async Shell Command buffer options."
+
+  ;; Don't ask if a new buffer should be created. Just do it!
+  (setq async-shell-command-buffer 'new-buffer)
+
+  ;; Don't show buffer with output for asynchronous commands
+  (add-to-list
+   'display-buffer-alist
+   '(".*Async Shell Command.*" (display-buffer-no-window))))
+
+
+(defun custom-exwm-config-wm-options ()
+  "Set misc Window Manager options."
+  ;; Set the initial workspace number.
+  (setq exwm-workspace-number 4)
+
+  ;; Make class name the buffer name
+  (add-hook 'exwm-update-class-hook
+            (lambda ()
+              (exwm-workspace-rename-buffer exwm-class-name))))
+
+
+(defun custom-exwm-config ()
+  "Custom configuration of EXWM."
+  (custom-exwm-config-wm-options)
+  (custom-exwm-config-shell-command)
+  (custom-exwm-config-global-keys)
+  (custom-exwm-config-editing-keys)
   (exwm-enable))
+
 
 (provide 'custom-exwm-config)
 ;;; custom-exwm-config ends here
