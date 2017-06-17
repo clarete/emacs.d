be6464c9eb8663101e78c6bb4d5562c527ed2b11
Lincoln de Sousa <lincoln@comum.org>
2017-06-16 21:57:33 -0400

    battery: Don't play sound if status is "Charging"

--- lisp/custom-battery.el
+++ lisp/custom-battery.el
@@ -11,12 +11,17 @@
 (defvar custom-battery-min-level 5
   "Minimum battery percentage before starting to beep.")
 
+(defun custom-battery-fmt (format)
+  "Shortcut for battery-format FORMAT."
+  (battery-format format (funcall battery-status-function)))
+
 (defun custom-battery-check ()
   "Check battery every 5 seconds and play a sound if it's low."
 
-  (let ((percentage (string-to-number
-                     (battery-format "%p" (funcall battery-status-function)))))
-    (if (< percentage custom-battery-min-level)
+  (let ((percentage (string-to-number (custom-battery-fmt "%p")))
+        (status (custom-battery-fmt "%B")))
+    (if (and (< percentage custom-battery-min-level)
+             (equal status "Discharging"))
         (progn
           (message "battery too low")
           (play-sound-file "~/.emacs.d/assets/sounds/beep.wav")))))
