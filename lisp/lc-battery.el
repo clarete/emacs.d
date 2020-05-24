617fdca3a625d32ecb03f0ceaa24cfb4567697f6
Lincoln de Sousa <lincoln@clarete.li>
2020-05-24 14:51:52 -0400

    battery: Normalize case before checking value

--- lisp/lc-battery.el
+++ lisp/lc-battery.el
@@ -8,22 +8,26 @@
 
 (require 'battery)
 
+
 (defvar lc/battery/min-level 5
   "Minimum battery percentage before starting to beep.")
+
 
 (defun lc/battery/fmt (format)
   "Shortcut for battery-format FORMAT."
   (battery-format format (funcall battery-status-function)))
+
 
 (defun lc/battery/check ()
   "Check battery every 5 seconds and play a sound if it's low."
   (let ((percentage (string-to-number (lc/battery/fmt "%p")))
         (status (lc/battery/fmt "%B")))
     (if (and (<= percentage lc/battery/min-level)
-             (equal status "Discharging"))
+             (equal (downcase status) "discharging"))
         (progn
           (message "battery too low")
           (play-sound-file "~/.emacs.d/assets/sounds/beep.wav")))))
+
 
 (defun lc/battery ()
   "Check battery and call itself in 5 sec."
